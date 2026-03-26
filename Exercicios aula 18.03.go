8c3c590ee1435c3a053bb3f8291aea36b9f33c4c
BalbinotBruno <naracybalbinot@gmail.com>
2026-03-25 21:43:09 -0300

    s

---
+++ Exercicios aula 18.03.go
@@ -0,0 +1,45 @@
+var num1 = 'Digite a nota'
+var num2 = 'Digite a nota'
+
+func{
+	nota = (num1 + num2)/2
+}
+
+if {
+	nota >= 7 
+	alert("O aluno for aprovado")
+} else {
+	alert("O aluno foi reprovado")
+}
+
+
+while {
+	var i = 0
+	i <= 20
+	i +2
+}
+
+
+inicio
+	numero 1 = "Digite o numero 1"
+	numero 2 = "Digite o numero 2"
+
+	media do aluno = (numero1+numero2)/2
+
+	se o aluno tiver (media >= 7)
+		mostre ("O aluno foi aprovado")
+
+		se nao ("O aluno foi reprovado")
+fim
+
+inicio
+	numero = 0
+	enquanto  algortitomo numero (<=20)
+	acresente (numero +2)
+	mostre numero
+fim
+
+
+
+
+	
