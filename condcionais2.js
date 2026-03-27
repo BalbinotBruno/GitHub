a627929885a824b1af080d0bb18ecd16916a43e3
BalbinotBruno <naracybalbinot@gmail.com>
2026-03-26 21:19:21 -0300

    Ajustes

--- condcionais2.js
+++ condcionais2.js
@@ -1,6 +1,16 @@
-let peso = 91.5
+let peso = 92
 let altura = 1.77
 
 let IMC = peso / (altura * altura)
 
 console.log( "Seu IMC é de: " + IMC)
+
+if(IMC <= 18){
+    console.log("Você está abaixo do peso!")
+} else if(IMC >= 19 && IMC <= 25){
+    console.log("Você esta no peso normal")
+} else if(IMC >= 26 && IMC <= 30){
+    console.log("Você esta com sobrepeso!")
+} else if(IMC > 30){
+    console.log("Você esta muito acima do peso")
+}
---
+++ estrutura_condicional.js
@@ -0,0 +1,16 @@
+switch (3){
+
+case 1 :
+console.log("cerveja");
+break;
+
+case 2 :
+console.log("Shopp");
+break;
+
+case 3 : 
+console.log("Suco");
+
+default :
+console.log("Vinho")
+}
