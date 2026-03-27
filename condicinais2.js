ff7e14f0dfc6ecbd70781f5196d37170e7f68ea7
BalbinotBruno <naracybalbinot@gmail.com>
2026-03-26 21:55:29 -0300

    Att

---
+++ condicinais2.js
@@ -0,0 +1,15 @@
+const bebidas = "alcoolicas"
+const item = "choop"
+
+    switch (alcoolicas){
+        case "cerveja" :
+            console.log("A escolha foi Cerveja")
+            break
+    
+        case  "choop" :
+            console.log("A escolha foi Chopp")
+            break
+    
+}
+
+
